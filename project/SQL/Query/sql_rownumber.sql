64236e682d24026e9ef23da04d513721b811c25e
sittiporn <sittiporn@silkspan.local>
2016-07-15 15:40:45 +0700

    Row_number order by

---
+++ project/SQL/Query/sql_rownumber.sql
@@ -0,0 +1,28 @@
+﻿
+
+ 
+
+declare @tb table(
+  product varchar(50),
+  unit int , 
+  price money
+)
+insert into @tb values('product1',2,'500')
+insert into @tb values('product2',1,'1500')
+insert into @tb values('product3',5,'1000')
+insert into @tb values('product2',2,'500')
+
+ 
+--- เรียงลำดับ
+  select
+	--row_number() over(partition by unit order by unit)  ord,
+	row_number() over(order by unit)  ord,
+	*
+  from @tb 
+
+
+--- order by ตาม produt 
+  select
+	 row_number() over(partition by product order by product)  ord, 
+	*
+  from @tb
