c6799ab397b913641267512ac5d6a3884e868693
unknown <Sittiporn@PROGRAMMER126.silkspan.local>
2016-07-29 15:47:11 +0700

    update sql sum count

---
+++ project/SQL/Query/sql_count.sql
@@ -0,0 +1,7 @@
+﻿
+-- sum count 
+select  sum(count(typedealer)) over() as 'cc'  from outbound_follow_partner
+--select typedealer,count(typedealer) from outbound_follow_partner
+where datediff(d,applicationdate,getdate()) = 1
+--group by typedealer
+--order by typedealer
